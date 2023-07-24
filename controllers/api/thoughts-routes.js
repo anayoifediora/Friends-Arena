a62da2a50fbe0c221357b78642eda8c59b86bf9f
Kanayo <anayoifediora@live.com>
2023-07-24 17:41:05 +0930

    completed thought routes

--- controllers/api/thoughts-routes.js
+++ controllers/api/thoughts-routes.js
@@ -45,3 +45,62 @@ router.post('/', async (req, res) => {
         res.status(500).json(err);
     }
 })
+
+// CREATE a reaction stored in a single thought's reactions array field
+
+router.post('/:thoughtId/reactions', async (req, res) => {
+
+    try {
+        const reactionData = await Reaction.create(req.body);
+        const thoughtData = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: reactionData._id } },
+            { runValidators: true, new: true }
+        );
+
+        if (!thoughtData) {
+            return res.status(404).json({ message: 'No thought found with this id!' });
+        }
+        res.json('Reaction created!');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// UPDATE a thought by its _id 
+
+router.put('/:thoughtId', async (req, res) => {
+
+    try {
+        const thoughtData = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            req.body,
+            { runValidators: true, new: true }
+        );
+
+        if (!thoughtData) {
+            return res.status(404).json({ message: 'No thought found with this id!' });
+        }
+        res.json('Thought updated!');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE a thought by its _id
+
+router.delete('/:thoughtId', async (req, res) => {
+
+    try {
+
+        const thoughtData = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+        if (!thoughtData) {
+            return res.status(404).json({ message: 'No thought found with this id!' });
+        }
+        res.json('Thought deleted!');
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
